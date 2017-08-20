59f8a80d35bcafa71e97b3d8c51e80779102bf6d
ivkis <ivanlazarevkis@gmail.com>
2017-08-20 15:48:59 +0300

    Initial Commit

---
+++ SoftCenterTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoftCenterTestProject
+//
+//  Created by IvanLazarev on 20/08/2017.
+//  Copyright © 2017 IvanLazarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
