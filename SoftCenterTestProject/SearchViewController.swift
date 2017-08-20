7c54f83847e5f25f5a5db9628ea76239cf05b612
ivkis <ivanlazarevkis@gmail.com>
2017-08-20 16:46:39 +0300

    SearchController UI

--- SoftCenterTestProject/SearchViewController.swift
+++ SoftCenterTestProject/SearchViewController.swift
@@ -10,8 +10,37 @@ import UIKit
 
 
 class SearchViewController: UIViewController {
+    var tableView: UITableView!
+    var searchBar: UISearchBar!
+
     override func loadView() {
         view = UIView()
         view.backgroundColor = UIColor.white
+
+        let segmentedControl = UISegmentedControl(items: ["iTunes", "GitHub"])
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+
+        searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(searchBar)
+
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+
+        view.addConstraints([
+            segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 20),
+            segmentedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
+            searchBar.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 20),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 20),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
