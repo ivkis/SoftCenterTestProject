b1ee80b836a8789e8aa102e487247abf3966ad75
ivkis <ivanlazarevkis@gmail.com>
2017-08-20 16:18:33 +0300

    Start an app without a storyboard

---
+++ SoftCenterTestProject/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  SoftCenterTestProject
+//
+//  Created by IvanLazarev on 20/08/2017.
+//  Copyright © 2017 IvanLazarev. All rights reserved.
+//
+
+import UIKit
+
+
+class SearchViewController: UIViewController {
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor.white
+    }
+}
